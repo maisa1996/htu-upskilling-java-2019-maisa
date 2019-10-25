e4a49cb4ba4021861dc158ab71f3f32c9f77eb65
maisa1996 <56797804+maisa1996@users.noreply.github.com>
2019-10-25 20:11:20 +0200

    Add files via upload

---
+++ Test4.java
@@ -0,0 +1,15 @@
+public class Test4{
+
+
+public static void main(String args[]){
+if (args.length==0){
+System.out.println("Please call java Test"+" with main argument atta");
+System.exit(0);
+
+}
+String name=args[0];
+System.out.println("Hello  "+name);
+
+}
+
+}
---
+++ TestForTest4.bat
@@ -0,0 +1,13 @@
+echo off 
+cls 
+javac Test4.java
+echo -----------------------
+echo expection Hello ata 
+java Test4 ata
+echo -----------------------
+echo expection Hello 
+java Test4 
+echo -----------------------
+echo expection mklsjnxjksjkcsjkjcsjkaJKcmklsjnxjksjkcsjkjcsjkaJKcmklsjnxjksjkcsjkjcsjkaJKc
+java Test4 mklsjnxjksjkcsjkjcsjkaJKcmklsjnxjksjkcsjkjcsjkaJKcmklsjnxjksjkcsjkjcsjkaJKcbk
+pause
