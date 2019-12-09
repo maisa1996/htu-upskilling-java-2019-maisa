d3b5481569146f5f3bc3e6d120fe92630f25a98a
maisa1996 <56797804+maisa1996@users.noreply.github.com>
2019-12-09 08:53:46 +0200

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,29 @@
+package hr;
+
+public class Employee {
+	 int employeeId;
+	
+	 private String employeeName;
+	 private double employeeSalary;
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+	 
+	
+	 
+}
